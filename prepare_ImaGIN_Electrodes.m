161099e98d87b549950542b8fa6a436e0e037ce6
viateur <viateur.tuyisenge@univ-grenoble-alpes.fr>
2018-11-20 12:35:48 +0100

    intermediate ImaGIN_Electrodes Cleanup

--- prepare_ImaGIN_Electrodes.m
+++ prepare_ImaGIN_Electrodes.m
@@ -14,49 +14,51 @@ S.Fname = fullfile(Root,[spm_str_manip(file,'r') '.mat']);
 if strcmp(source,'classic')
     S.filenamePos = fullfile(implantationFile,'Electrodes_Pos_MNI.txt');
     S.filenameName = fullfile(implantationFile,'Electrodes_Name.txt');
+elseif strcmp(source, 'intranat')
+    S.filenameName = implantationFile;
 end
 
-%for files generated with Intranat
-if strcmp(source, 'intranat')
-    data=extractIntranatFile(implantationFile);
-    for i1=1:size(data,2)
-        for i2=1:size(data{i1},1)
-            if strcmp(data{i1}{i2},'MNI')
-                indi=i1;
-                indj=i2;
-            end
-        end
-    end
-    positiontmp = data{indi};
-    nametmp = data{1};
-    Position = [];
-    Name = cell(1,1);
-    if strcmp(polarity,'monopolar')
-        for i=(indj+1):length(nametmp)
-            if isempty(strfind(nametmp{i},'-'))
-                
-                iLastLetter = find(~ismember(nametmp{i}, '0123456789'), 1, 'last');
-                if isempty(iLastLetter)
-                    iLastLetter = 0;
-                end
-                
-                if iLastLetter > 0
-                    name     = [lower(nametmp{i}(1:iLastLetter)) nametmp{i}(iLastLetter+1:end)];
-                    Name{1}  = cat(1,Name{1},{name});
-                    Position = cat(1,Position,str2num(positiontmp{i}));
-                end
-            end
-        end
-    else
-        if strcmp(polarity, 'bipolar')
-            % TODO: Throw an exception here, don't execute anything!!!
-            disp('Bipolar option not implemented')
-        end
-    end
-S.Position = Position;
-S.Name = Name;
-end
-
+% %for files generated with Intranat
+% if strcmp(source, 'intranat')
+%     data=extractIntranatFile(implantationFile);
+%     for i1=1:size(data,2)
+%         for i2=1:size(data{i1},1)
+%             if strcmp(data{i1}{i2},'MNI')
+%                 indi=i1;
+%                 indj=i2;
+%             end
+%         end
+%     end
+%     positiontmp = data{indi};
+%     nametmp = data{1};
+%     Position = [];
+%     Name = cell(1,1);
+%     if strcmp(polarity,'monopolar')
+%         for i=(indj+1):length(nametmp)
+%             if isempty(strfind(nametmp{i},'-'))
+%                 
+%                 iLastLetter = find(~ismember(nametmp{i}, '0123456789'), 1, 'last');
+%                 if isempty(iLastLetter)
+%                     iLastLetter = 0;
+%                 end
+%                 
+%                 if iLastLetter > 0
+%                     name     = [lower(nametmp{i}(1:iLastLetter)) nametmp{i}(iLastLetter+1:end)];
+%                     Name{1}  = cat(1,Name{1},{name});
+%                     Position = cat(1,Position,str2num(positiontmp{i}));
+%                 end
+%             end
+%         end
+%     else
+%         if strcmp(polarity, 'bipolar')
+%             % TODO: Throw an exception here, don't execute anything!!!
+%             error('Bipolar option not implemented')
+%         end
+%     end
+% S.Position = Position;
+% S.Name = Name;
+% end
+% 
 
 S.FileTxtOut = FileTxtOut;
 S.FileOut = FileOut;
@@ -71,12 +73,12 @@ close all
 
 end
 
-% It gets the CSV file, and reads its content to Matlab structure 'data'
-function data = extractIntranatFile(filename)
-delimiter = '\t';
-formatSpec = '%s%s%s%s%s%s%s%s%s%s%s%s%s%s%[^\n\r]';
-
-fileID = fopen(filename,'r');
-data = textscan(fileID, formatSpec, 'Delimiter', delimiter,  'ReturnOnError', false);
-fclose(fileID);
-end
+% % It gets the CSV file, and reads its content to Matlab structure 'data'
+% function data = extractIntranatFile(filename)
+% delimiter = '\t';
+% formatSpec = '%s%s%s%s%s%s%s%s%s%s%s%s%s%s%[^\n\r]';
+% 
+% fileID = fopen(filename,'r');
+% data = textscan(fileID, formatSpec, 'Delimiter', delimiter,  'ReturnOnError', false);
+% fclose(fileID);
+% end
