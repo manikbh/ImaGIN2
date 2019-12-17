29f4192386a1aa1507498ace0461eebf0373f671
Anthony B <TEST@mail.COM>
2019-12-17 16:15:41 +0100

    Blank space to force compilation

--- prepare_ImaGIN_trainBaseUpdate.m
+++ prepare_ImaGIN_trainBaseUpdate.m
@@ -1,5 +1,6 @@
 function prepare_ImaGIN_trainBaseUpdate(inDir)
-% FileIn: path linking to the Cropped MEEG object 
+% FileIn: path linking to the Cropped MEEG object
+
 clear S;
 S.DirIn = inDir;
 ImaGIN_trainBaseUpdate(S)
