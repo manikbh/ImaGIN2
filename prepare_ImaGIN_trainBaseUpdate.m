f40f1ff65c35feaf7de383175fd6e3c06923daae
Anthony B <TEST@mail.COM>
2019-12-20 18:39:10 +0100

    Correct the input assignation

--- prepare_ImaGIN_trainBaseUpdate.m
+++ prepare_ImaGIN_trainBaseUpdate.m
@@ -1,13 +1,8 @@
 function prepare_ImaGIN_trainBaseUpdate(inDir)
 % FileIn: path linking to the Cropped MEEG object
 clear S;
-S.DirIn = inDir;
-ImaGIN_trainBaseUpdate(S);   
-f_id = fopen('/home/anthonyboyer/Documents/Debugs/20122019/ouput/log.txt','w');
-fprintf(f_id,inDir);
-fclose(f_id);
-
-close all
-
+S = fullfile(inDir);
+ImaGIN_trainBaseUpdate(S);  
+close all;
 end
 
